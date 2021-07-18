b0a6df3cdca3a6674db7ac450a296444d10512af
dawnbergdev <mmos7239@gmail.com>
2021-07-18 05:09:27 +0200

    signup Done

--- job/accounts/views.py
+++ job/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import signupForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form = signupForm(request.POST)
+
+        if form.is_valid:
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username = username , password=password)
+            login(request,user)
+            return redirect('accounts/profile')
+    else:
+        form = signupForm    
+
+    return render(request,'registration/signup.html' , {'form':form})
--- job/job/urls.py
+++ job/job/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/',include('accounts.urls') ),
     path('admin/', admin.site.urls),
     path('jobs/', include('Jobs.urls', namespace='jobs')),
    
